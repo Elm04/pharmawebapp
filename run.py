9a7d1fc7fee2095a3a04699859cf0a9097bd424a
Elm04 <elimuya10@gmail.com>
2025-07-07 06:52:25 +0200

    mainapp to create_app

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-from pharmaweb import mainapp
+from pharmaweb import create_app
 
 print("### EXECTUTION DU TEST DE MON APP###")
 # Crée l'instance Flask en passant la configuration
-main = mainapp(config_class='config.Config')  # Adaptez ceci à votre classe de config réelle
+main = create_app(config_class='config.Config')  # Adaptez ceci à votre classe de config réelle
