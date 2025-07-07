255619faadb53b36f1c298d8fafab3cd6b6fef82
Elm04 <elimuya10@gmail.com>
2025-07-07 06:22:10 +0200

    la config dans run

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from pharmaweb import mainapp
 
-app = mainapp()
-
+# Crée l'instance Flask en passant la configuration
+app = mainapp(config_class='config.Config')  # Adaptez ceci à votre classe de config réelle
