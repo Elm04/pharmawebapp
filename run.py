4511ee60c7f728c4ce82d1f5c5a218aca60a06b1
Elm04 <elimuya10@gmail.com>
2025-07-07 13:27:37 +0200

    set run for deployement

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from pharmaweb import mainapp
 
 app = mainapp()
 
-if __name__ =="__main__":
-    app.run(debug=True, host="0.0.0.0")
+# if __name__ =="__main__":
+#     app.run(debug=True, host="0.0.0.0")
